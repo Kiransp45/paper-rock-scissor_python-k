e98932651bdf4272a37b1d728c728bc4f85a8fbc
Kiransp45 <160218747+Kiransp45@users.noreply.github.com>
2024-02-17 01:04:22 -0800

    first program

---
+++ prs.py
@@ -0,0 +1,98 @@
+print("Hello world")
+import random
+print("**********************************************************************")
+print("***     *** ** * PAPER *** ROCK *** SCISSOR * ** ***     ***")
+print("***********************************************************************")
+print(" lets start the game!!!!!!!............. ")
+point=0
+ai=0
+hs=0
+cs=0
+ma=0
+while(True):
+    if(ma>5):
+        print("##############################################################################################################################")
+        print("total match",ma)
+        print("human score",hs)
+        print("AI score",cs)
+        if('hs>vs'):
+            print("congrats you won")
+        elif('cs>hs'):
+            print("you lost.....better luck mnext time")
+        else:
+            print("match draw")
+        print("##############################################################################################################################")
+        break
+    c=input(" choose the option: paper->p Rock->r Scissor->s stop->stop ")
+    if(c=="p"):
+        print("paper")
+        point=10+random.randint(1,3)
+        match point:
+          case 11:
+            ma=ma+1
+            hs=hs+1
+            print("human: paper","AI: rock")
+            print("match:",ma,"human score",hs,"AI score",cs)
+            print("human wins","AI lost the match")
+          case 12:
+            ma=ma+1
+            cs=cs+1
+            print("human: paper","AI: scissor")
+            print("match:",ma,"human score",hs,"AI score",cs)
+            print("human lost","AI won the match")
+          case 13:
+            ma=ma+1
+            hs=hs+1
+            cs=cs+1
+            print("human: paper","AI: paper")
+            print("match:",ma,"human score",hs,"AI score",cs)
+            print("match draw")
+    elif(c=="r"):
+        print("rock")
+        point=20+random.randint(1,3)
+        match point:
+          case 21:
+            ma=ma+1
+            hs=hs+1
+            print("human: rock","AI: scissor")
+            print("match:",ma,"human score",hs,"AI score",cs)
+            print("human wins","AI lost the match")
+          case 22:
+            ma=ma+1
+            cs=cs+1
+            print("human: rock","AI: paper")
+            print("match:",ma,"human score",hs,"AI score",cs)
+            print("human lost","AI won the match")
+          case 23:
+            ma=ma+1
+            hs=hs+1
+            cs=cs+1
+            print("human: rock","AI: rock")
+            print("match:",ma,"human score",hs,"AI score",cs)
+            print("match draw")
+    elif(c=="s"):
+        print("scissor")
+        point=30+random.randint(1,3)
+        match point:
+          case 31:
+            ma=ma+1
+            hs=hs+1
+            print("human: scissor","AI: paper")
+            print("match:",ma,"human score",hs,"AI score",cs)
+            print("human wins","AI lost the match")
+          case 32:
+            ma=ma+1
+            cs=cs+1
+            print("human: scissor","AI: rock")
+            print("match:",ma,"human score",hs,"AI score",cs)
+            print("human lost","AI won the match")
+          case 33:
+            ma=ma+1
+            hs=hs+1
+            cs=cs+1
+            print("human: scissor","AI: scissor")
+            print("match:",ma,"human score",hs,"AI score",cs)
+            print("match draw")
+    elif(c=="stop"):
+        break
+print("program end")
